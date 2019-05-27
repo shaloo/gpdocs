891400b7a6e40c7b92e34258a74931df3057a237
shaloo <shalz@hotmail.com>
2019-05-27 16:27:21 +0530

    rtd theme conditional

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,8 +48,14 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes", ]
+#html_theme = 'sphinx_rtd_theme'
+#html_theme_path = ["_themes", ]
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
