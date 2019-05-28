10625856668e21204b0850715f1dada888b529d5
shaloo <shalz@hotmail.com>
2019-05-28 15:43:31 +0530

    fixed rtd theme contents.rst issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -56,7 +57,6 @@ if on_rtd:
     html_theme = 'default'
 else:
     html_theme = 'sphinx_rtd_theme'
-    master_doc = 'index'
 
 html_logo = 'img/genpipes-doc-img.png'
 
